5468a8a3a0ab42af1b0daa0f8621774cc453370c
James Zhu <zhujian0805@gmail.com>
2015-08-22 21:17:14 +0800

    mod

---
+++ syntax/main-in-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import sys
+from test import main
+
+if __name__ == "__main__":
+  sys.exit(main())
---
+++ syntax/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import sys
+
+def main():
+  print sys.argv[1:]
+
+if __name__ == "__main__":
+  main()
