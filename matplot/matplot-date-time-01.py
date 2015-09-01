8a2e89a06b8faaa5b91c01690e66d1321aa5cd55
James Zhu <zhujian0805@gmail.com>
2015-09-01 21:06:57 +0800

    mod

--- matplot/matplot-date-time-01.py
+++ matplot/matplot-date-time-01.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
 import numpy as np
 import datetime as dt
 import time
-from pprint import pprint
 
 n=20
-duration=10000
+duration=1000
 now=time.mktime(time.localtime())
 timestamps=np.linspace(now,now+duration,n)
-pprint(timestamps)
 dates=[dt.datetime.fromtimestamp(ts) for ts in timestamps]
 values=np.sin((timestamps-now)/duration*2*np.pi)
-plt.subplots_adjust()
-plt.xticks( rotation=1 )
+plt.subplots_adjust(bottom=0.2)
+plt.xticks( rotation=25 )
 ax=plt.gca()
 xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
 ax.xaxis.set_major_formatter(xfmt)
-plt.plot(dates, values)
+plt.plot(dates,values)
 plt.show()
