0c6803676f82cb38d89311551f2556314d436bd6
James Zhu <zhujian0805@gmail.com>
2015-09-01 21:30:58 +0800

    mod

--- matplot/Readme.txt
+++ matplot/Readme.txt
@@ -1,2 +1,4 @@
 [Examples]
 http://stackoverflow.com/questions/4090383/plotting-unix-timestamps-in-matplotlib
+
+http://stackoverflow.com/questions/1574088/plotting-time-in-python-with-matplotlib
---
+++ matplot/matplot-date-time-02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import datetime
+import random
+import matplotlib.pyplot as plt
+
+# make up some data
+x = [datetime.datetime.now() + datetime.timedelta(hours=i) for i in range(12)]
+y = [i+random.gauss(0,1) for i,_ in enumerate(x)]
+
+# plot
+plt.plot(xsynchronized)
+# beautify the x-labels
+plt.gcf().autofmt_xdate()
+
+plt.show()
