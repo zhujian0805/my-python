b14e6df0e41b1a77662bffcc7c1f136971401818
James Zhu <zhujian0805@gmail.com>
2015-09-05 17:29:20 +0800

    mod

--- unicode/setdefaultencodeing-01.py
+++ unicode/setdefaultencodeing-01.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Set default encoding here, now your python code can process utf8 characters
+# Set default encoding here, now your can process utf8 characters with in the python code
 import sys  
 reload(sys)  
 sys.setdefaultencoding('utf8')   
