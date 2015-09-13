99a97e6eb5d7ee86bbb253245bbf7d2df48b21f4
James Zhu <zhujian0805@gmail.com>
2015-09-13 20:41:47 +0800

    mod

---
+++ features/closure-02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f(j):
+            def g():
+                return j*j
+            return g
+        r = f(i)
+        fs.append(r)
+    return fs
+f1, f2, f3 = count()
+print count()
+print f1(), f2(), f3()
