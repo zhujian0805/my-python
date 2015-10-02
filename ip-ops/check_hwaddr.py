403b4afcf2121695528c5d989302d210f7b5cd08
James Zhu <zhujian0805@gmail.com>
2015-10-02 21:46:33 +0800

    mod

--- ip-ops/check_hwaddr.py
+++ ip-ops/check_hwaddr.py
@@ -1,10 +1,11 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use Socket;
-require 'sys/ioctl.ph';
+#!/usr/bin/python
+import socket
+import fcntl
+import struct
 
 def getHwAddr(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
-            return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+  return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+
+print getHwAddr('eth0')
