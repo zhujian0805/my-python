be5189c63663d12b481fa18072e8f8a4f2f99b9f
James Zhu <zhujian0805@gmail.com>
2015-10-15 11:10:58 +0800

    mod

--- ip-ops/check_hwaddr.py
+++ ip-ops/check_hwaddr.py
@@ -8,4 +8,6 @@ def getHwAddr(ifname):
   info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
   return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
 
-print getHwAddr('eth0')
+
+if __name__ == '__main__':
+  print getHwAddr('eth0')
