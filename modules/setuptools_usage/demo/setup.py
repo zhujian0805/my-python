a6eb65f73e9fb92e531ecb346dc772a0c5b7cf18
James Zhu <zhujian0805@gmail.com>
2015-10-18 13:13:21 +0800

    ?? demo/

---
+++ modules/setuptools_usage/demo/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+setup(
+    name = "demo",
+    version = "0.1",
+
+    # Including all packages in src 
+    packages = find_packages('src'),
+    # Telling distutils packages are in src 
+    package_dir = {'':'src'},
+
+    package_data = {
+      # Including files with txt suffix 
+      '': ['*.txt'],
+      # Including all data files in data
+      'demo': ['data/*.dat'],
+    },
+
+    entry_points = {
+        'console_scripts': [
+            'foo = demo:test',
+            'bar = demo:test',
+        ],
+        'gui_scripts': [
+            'baz = demo:test',
+        ]
+    }
+
+)
