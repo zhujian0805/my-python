8bfc2eb1aa13377b3ff8bb2e8d0377e46b606190
Nemo Zhu <zhujian0805@gmail.com>
2015-10-31 18:00:20 +0800

    ?? github-oauth-tutorial.py

---
+++ oauth-authentication/github-oauth-tutorial.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Credentials you get from registering a new application
+client_id = '2607467f409a6ff2c25d'
+client_secret = '73f30950b9a89ccb665bf75e3a432d26c7da815d'
+
+# OAuth endpoints given in the GitHub API documentation
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
+
+from requests_oauthlib import OAuth2Session
+github = OAuth2Session(client_id)
+
+# Redirect user to GitHub for authorization
+authorization_url, state = github.authorization_url(authorization_base_url)
+print 'Please go here and authorize,', authorization_url
+
+# Get the authorization verifier code from the callback url
+redirect_response = raw_input('Paste the full redirect URL here:')
+
+# Fetch the access token
+github.fetch_token(token_url, client_secret=client_secret,
+        authorization_response=redirect_response)
+
+# Fetch a protected resource, i.e. user profile
+r = github.get('https://api.github.com/user/repos')
+print r.content
