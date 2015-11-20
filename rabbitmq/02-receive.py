48ea2c2eedb24eb034e8f5602704cf606c5ff32a
Nemo Zhu <zhujian0805@gmail.com>
2015-11-20 19:32:48 +0800

    M 02-receive.py

--- rabbitmq/02-receive.py
+++ rabbitmq/02-receive.py
@@ -2,7 +2,8 @@
 import pika
 import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters( host='ip'))
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='ip'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange=sys.argv[1], type='topic', durable=True)
@@ -11,10 +12,10 @@ result = channel.queue_declare(exclusive=True, durable=True)
 
 queue_name = result.method.queue
 
-
 channel.queue_bind(exchange=sys.argv[1], queue=sys.argv[2])
 
 print ' [*] Waiting for logs. To exit press CTRL+C'
+
 
 def callback(ch, method, properties, body):
     print " [x] %r:%r" % (method.routing_key, body,)
