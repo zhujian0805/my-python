629bbb2bd01ef9d2f2476b9817ed77d320de31d3
Nemo Zhu <zhujian0805@gmail.com>
2015-12-26 19:16:02 +0800

    ?? var-2-unicode-to-utf8.py

---
+++ modules/unicode/var-2-unicode-to-utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Checkout this thread: http://stackoverflow.com/questions/10268518/python-string-to-unicode
+# I get a utf8 string from the internet, so it is actually a string of a lot of
+# characters: '\', 'u', .....
+string = '\u738b\u5e9c\u4e95\u5e97\u53cc\u4eba\u5957\u9910\uff01\u8282\u5047\u65e5\u901a\u7528\uff0c\u63d0\u4f9b\u514d\u8d39WiFi\uff01'
+
+# To make unicode out of this, use decode('unicode-escape')
+
+print string.decode('unicode-escape').encode('utf8')
