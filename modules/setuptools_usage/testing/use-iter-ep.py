fc873703801bdb8380e5fe02edf62c98a0d0044a
Nemo Zhu <zhujian0805@gmail.com>
2016-01-27 23:31:14 +0800

    M use-iter-ep.py

--- modules/setuptools_usage/testing/use-iter-ep.py
+++ modules/setuptools_usage/testing/use-iter-ep.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 from pkg_resources import iter_entry_points
-for entry_point in iter_entry_points(group='cms.plugin', name=None):
+for entry_point in iter_entry_points('cms.plugin'):
     print(entry_point)
