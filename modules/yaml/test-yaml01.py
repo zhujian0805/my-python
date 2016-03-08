4f87076f9a2ab39346c20f743136c9b564141723
James Zhu <zhujian0805@gmail.com>
2016-03-08 18:18:53 +0800

    M test-yaml01.py

--- modules/yaml/test-yaml01.py
+++ modules/yaml/test-yaml01.py
@@ -2,3 +2,25 @@
 
 import yaml
 
+yyy = yaml.load("""
+---
+# An employee record
+name: Martin D'vloper
+job: Developer
+skill: Elite
+employed: True
+foods:
+    - Apple
+    - Orange
+    - Strawberry
+    - Mango
+languages:
+    ruby: Elite
+    python: Elite
+    dotnet: Lame
+                """)
+
+print yyy
+
+
+print yaml.dump( { 'name': "James Zhu", 'family': [ 'Nemo', "lily" ] } )
