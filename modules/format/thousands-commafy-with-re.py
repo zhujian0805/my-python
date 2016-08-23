3410d3bf66e52c783b07a6c7c40fbeb268746290
James Zhu <zhujian0805@gmail.com>
2016-08-23 14:22:51 +0800

    add

---
+++ modules/format/thousands-commafy-with-re.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import re
+def strConv(s):  
+    s =  str(s)
+    while True:
+        (s,count) = re.subn(r"(\d)(\d{3})((:?,\d\d\d)*)$",r"\1,\2\3",s)
+        if count == 0 : break
+    return s
+print strConv(12345)
