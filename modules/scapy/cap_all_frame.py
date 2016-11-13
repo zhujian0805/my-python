2cd55122e7f1a08dfbf9222b5dd9625f0928f8b5
James Zhu <zhujian0805@gmail.com>
2016-11-13 17:38:22 +0800

    A  scapy/cap_all_frame.py

---
+++ modules/scapy/cap_all_frame.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from scapy.all import *
+import sys
+
+def pkt_callback(pkt):
+    pkt.show() # debug statement
+
+sniff(iface=sys.argv[1], prn=pkt_callback, filter="ip", store=0)
