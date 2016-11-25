4f002804e3fa0cdba7a36cbe912f107b03b289bb
James Zhu <zhujian0805@gmail.com>
2016-11-25 10:57:39 +0800

    A  udp_client.py

---
+++ network-related/udp_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import socket
+
+target_host = "127.0.0.1"
+target_port = 67
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send some data
+client.sendto("AAABBBCCC",(target_host,target_port))
+
+# receive some data
+data, addr = client.recvfrom(4096)
+print data
