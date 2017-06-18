9528778435cc5a3c035b826ca85ba38cf13de347
James <zhujian0805@gmail.com>
2017-06-18 10:52:19 +0800

    This commit has the following change(s): ------------------------------------------------  M var-as-key-for-dict.py ------------------------------------------------

--- jinja2/var-as-key-for-dict.py
+++ jinja2/var-as-key-for-dict.py
@@ -2,8 +2,11 @@
 
 from jinja2 import Template
 
-v = { 'k1': 'one', 'k2': 'two' }
-k = [ 'k1', 'k2' ]
+v = {'k1': 'one', 'k2': 'two'}
+k = ['k1', 'k2']
 
 template = Template('{{ a[b[0]] }}')
-print template.render(a=v,b=k)
+print(template.render(a=v, b=k))
+
+template = Template('{% for i in b %} {{ a[i] }} {% endfor %}')
+print(template.render(a=v, b=k))
