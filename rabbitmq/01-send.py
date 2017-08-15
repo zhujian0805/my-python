1eeb49e0dcf4da2f3ec160b80fc3ba60eed71bd1
James Zhu <abc@def.com>
2017-08-15 13:51:52 +0800

    This commit has the following change(s): ------------------------------------------------ M  01-send.py ------------------------------------------------

--- rabbitmq/01-send.py
+++ rabbitmq/01-send.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import pika
 import sys
+from pika import credentials as pika_credentials
 
-connection = pika.BlockingConnection(pika.ConnectionParameters( host=sys.argv[4]))
+conn_parameters = pika.ConnectionParameters(host=sys.argv[1], port=int(sys.argv[2]), credentials=pika_credentials.PlainCredentials(sys.argv[3], sys.argv[4]))
+
+connection = pika.BlockingConnection(conn_parameters)
 
 channel = connection.channel()
 
@@ -10,8 +13,8 @@ channel.exchange_declare(exchange='testing.001', type='topic', durable=True, aut
 
 #channel.queue_declare(queue='testing', durable=True)
 
-channel.basic_publish(exchange=sys.argv[1], routing_key=sys.argv[2], body=sys.argv[3], properties=pika.BasicProperties(delivery_mode = 2,))
+channel.basic_publish(exchange=sys.argv[5], routing_key=sys.argv[6], body=sys.argv[7], properties=pika.BasicProperties(delivery_mode = 2,))
 
-print " [x] Sent %s " % sys.argv[3]
+print " [x] Sent %s " % sys.argv[7]
 
 connection.close()
