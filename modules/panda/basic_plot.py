309dd2359f8142040fcdcbe4a5a5490e52643cb4
James Zhu <zhujian0805@gmail.com>
2017-09-08 21:52:17 +0800

    This commit has the following change(s): ------------------------------------------------ A  panda/basic_plot.py ------------------------------------------------

---
+++ modules/panda/basic_plot.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import  matplotlib.pyplot as plt
+
+df = pd.DataFrame(np.random.randn(10,4),index=pd.date_range('1/1/2000',
+   periods=10), columns=['Col1', 'Col2', 'Col3', 'Col4'])
+
+df.plot()
+
+plt.show()
