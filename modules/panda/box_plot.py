de85bc1f33b5226a8b772d656cdd676e36b3a32d
James Zhu <zhujian0805@gmail.com>
2017-09-08 21:55:06 +0800

    This commit has the following change(s): ------------------------------------------------ A  box_plot.py ------------------------------------------------

---
+++ modules/panda/box_plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import pandas as pd
+import numpy as np
+import  matplotlib.pyplot as plt
+
+df = pd.DataFrame(np.random.rand(10, 5), columns=['A', 'B', 'C', 'D', 'E'])
+df.plot.box()
+
+plt.show()
