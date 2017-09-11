1afdf6e698a5e3e5dc3e2f687aabe29c7da25993
James Zhu <zhujian0805@gmail.com>
2017-09-11 21:22:37 +0800

    This commit has the following change(s): ------------------------------------------------ A  get-vm-hostnames.py  M ../xml/namespace-xml.py ------------------------------------------------

---
+++ modules/libvirt/get-vm-hostnames.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from __future__ import print_function
+import sys
+import libvirt
+from pprint import pprint as pp
+import xml.etree.ElementTree as ET
+
+conn = libvirt.open('qemu:///system')
+if conn == None:
+    print('Failed to open connection to qemu:///system', file=sys.stderr)
+    exit(1)
+
+dom = conn.lookupByName('instance-00029ff0')
+if dom == None:
+    print('Failed to find the domain '+domName, file=sys.stderr)
+    exit(1)
+
+for i in conn.listAllDomains():
+  raw_xml = i.XMLDesc(0)
+  root = ET.fromstring(raw_xml)
+
+  for child in root.findall('metadata'):
+    for c in child.findall("{http://openstack.org/xmlns/libvirt/nova/1.0}instance"):
+        for cc in c:
+            if cc.tag == '{http://openstack.org/xmlns/libvirt/nova/1.0}name':
+ 	    	print(cc.tag,cc.text)
