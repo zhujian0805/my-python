cb94aee194e3322ebb04c525c44a754f57d25f98
James Zhu <zhujian0805@gmail.com>
2018-05-08 20:43:27 +0800

    This commit has the following change(s): ------------------------------------------------ A  scope/scope.py ------------------------------------------------

---
+++ syntax/scope/scope.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+#===============================================================================
+#
+#         FILE: scope.py
+#
+#        USAGE: ./scope.py
+#
+#  DESCRIPTION:
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: James Zhu (), zhujian0805@gmail.com
+# ORGANIZATION: ZJ
+#      VERSION: 1.0
+#      CREATED: Tue 08 May 2018 08:33:20 PM CST
+#     REVISION: ---
+#===============================================================================
+
+foo = 'MODULE level string'
+
+def aaa():
+    foo = "first level in FUNC aaaa"
+    print(foo)
+
+    def bbb():
+        foo = "Second level in FUNC bbbb"
+        print(foo)
+
+    def ccc():
+        print(foo)
+
+    bbb()
+    ccc()
+
+aaa()
