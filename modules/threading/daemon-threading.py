071bbbc9dacc3b9b9689c494f15d56498e696acd
James Zhu <zhujian0805@gmail.com>
2018-06-24 12:33:58 +0800

    This commit has the following change(s): ------------------------------------------------  M daemon-threading.py ------------------------------------------------

--- modules/threading/daemon-threading.py
+++ modules/threading/daemon-threading.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
+"""
+This is for learning threading
+"""
 import threading
 import time
 
-def nondaemonThread():
-   print("starting my thread")
-   time.sleep(8)
-   print("ending my thread")
+def nonDaemonThread():
+    """ Testing """
+    print("starting my thread")
+    time.sleep(8)
+    print("ending my thread")
 def daemonThread():
-   while True:
-    print("Hello")
-    time.sleep(2)
+    """ Testing """
+    while True:
+        print("Hello")
+        time.sleep(2)
 if __name__ == '__main__':
-   nondaemonThread = threading.Thread(target = nondaemonThread)
-   daemonThread = threading.Thread(target = daemonThread)
-   daemonThread.setDaemon(True)
-   daemonThread.start()
-   nondaemonThread.start()
+    nonDaemonThread = threading.Thread(target=nonDaemonThread)
+    daemonThread = threading.Thread(target=daemonThread)
+    daemonThread.setDaemon(True)
+    daemonThread.start()
+    nonDaemonThread.start()
